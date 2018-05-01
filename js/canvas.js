916723654d7a4a065d0e97729efa939b3df55ecf
Hshibuya <hiroki.shibuya96@gmail.com>
2018-05-01 04:28:27 -0400

    First FTP clone

--- js/canvas.js
+++ js/canvas.js
@@ -1,22 +1,19 @@
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
-context.canvas.width = window.innerWidth;
-context.canvas.height = window.innerHeight;
 var drawing = false;
-var color = "black";
-var size = 5;
+var color1 = "black";
+var color2 = "black";
+var size = 10;
 var brush = "square";
+var firstClick = false;
 var img = document.getElementById("spbob");
+var initImg = document.getElementById("init");
 img.style.display="none";
+initImg.style.display="none";
 
-function drawCanvas(x, y, sizeX, sizeY){
-    context.globalAlpha = 0.2;
-    context.fillStyle = "white";
-    context.fillRect(x, y, sizeX, sizeY);
-    context.globalAlpha = 1.0;
-}
-drawCanvas(0, 0, canvas.width, canvas.height);
+context.drawImage(initImg, 0, 0, canvas.width, canvas.height);
 context.fillStyle = "black";
+context.strokeStyle = "black";
 
 canvas.addEventListener("mousemove", mouseMove);
 canvas.addEventListener("mousedown", mouseDown);
@@ -26,16 +23,28 @@ function mouseMove(event){
     if(drawing==true){
         draw(event);
     }
+    if(firstClick==false){
+        firstClick=true;
+        clearScreen();
+    }
 }
 
-function changeColor(){
-    color = document.getElementById("color").value;
-    context.fillStyle=color;
+function changeColor1(){
+    color1 = document.getElementById("color1").value;
+    context.fillStyle=color1;
+}
+
+function changeColor2(){
+    color2 = document.getElementById("color2").value;
+    context.strokeStyle=color2;
 }
 
 function changeSize(){
     var newSize = prompt("What is your new size")
     size = parseInt(newSize)
+    if(size%2==1){
+        size-=1
+    }
 }
 
 function changeBrush(){
@@ -50,17 +59,12 @@ function draw(event){
     var x = event.pageX-canvas.offsetLeft;
     var y = event.pageY-canvas.offsetTop;
     if(brush=="square"){
+        context.strokeStyle=color2;
         context.fillRect(x-size/2, y-size/2, size, size)
+        context.strokeRect(x-size/2, y-size/2, size, size)
     }
     else if(brush=="circle"){
-        context.strokeStyle=color;
-        context.beginPath();
-        context.arc(x, y, size, 0, 2*Math.PI, false);
-        context.fill();
-        context.stroke();
-    }
-    else if(brush=="slinky"){
-        context.strokeStyle="black";
+        context.strokeStyle=color2;
         context.beginPath();
         context.arc(x, y, size, 0, 2*Math.PI, false);
         context.fill();
@@ -68,7 +72,6 @@ function draw(event){
     }
     else if(brush=="eraser"){
         context.clearRect(x-size/2,y-size/2, size, size);
-        drawCanvas(x-size/2,y-size/2,size, size);
     }
     else{
         context.drawImage(img, x-size/2, y-size/2, size, size*(393/470));
@@ -80,12 +83,20 @@ function mouseDown(event){
     draw(event);
 }
 
-function saveImage(){
+function downloadImage(){
     var image = canvas.toDataURL();
-    window.location.href=image;
+    var element = document.createElement('a');
+    element.setAttribute('href', image);
+    element.setAttribute('download', 'image.png');
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
 }
 
 function clearScreen(){
     context.clearRect(0, 0, canvas.width, canvas.height);
-    drawCanvas(0,0,canvas.width,canvas.height);
+    
 }
