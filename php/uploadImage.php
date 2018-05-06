b4f0a198b833ea88e62e813d0162b534b1c755a6
Hshibuya <hiroki.shibuya96@gmail.com>
2018-05-06 13:00:06 -0400

    ajax4

--- php/uploadImage.php
+++ php/uploadImage.php
@@ -9,15 +9,58 @@ if($paintings){
 }
 
 $imgData = $_POST['imageurl'];
+if(isset($_POST['name'])){
+	$name = $_POST['name'];
+}
+else{
+	$name = 'unknown';
+}
+if(isset($_POST['secret'])){
+	$secret = $_POST['secret'];
+}
+else{
+	$secret = 'unknown';
+}
+if(isset($_POST['title'])){
+	$title = $_POST['title'];
+}
+else{
+	$title = 'untitled';
+}
+if(isset($_POST['desc'])){
+	$desc = $_POST['desc'];
+}
+else{
+	$desc = 'no description'
+}
+$date = G
+
 $imgData = substr($imgData,strpos($imgData,",")+1);
 $imgData = str_replace(' ', '+', $imgData);
 $img = base64_decode($imgData);
 
-$filename = $img_dir.'/'.$file_count.'.png';
+$fileData = $file_count.$date.$name.$secret.$title;
+$fileHash = hash('sha256', fileData);
+$filename = $img_dir.'/'.$fileHash.'.png';
 echo chmod($img_dir,0755);
 
 echo "$filename<br/>";
 
 $success = file_put_contents($filename, $img);
 
-?>
+
+
+$servername = 'localhost';
+$username = 'id3110622_hshibuya96';
+$password = '0511myh77';
+$dbname = 'id3110622_paintings'
+
+$connection = new mysqli($servername, $username, $password);
+$sql = "INSERT INTO paintings (pid, name, secret, descr, date) VALUES ($fileHash, $name, $secret, $desc, CURRENT_TIMESTAMP)";
+
+if ($connection->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
