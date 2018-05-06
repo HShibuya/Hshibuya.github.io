5a3b38ee1f89705b32e3f977afbf4d0fb20746da
Hshibuya <hiroki.shibuya96@gmail.com>
2018-05-06 07:26:43 -0400

    test

--- php/uploadImage.php
+++ php/uploadImage.php
@@ -1,16 +1,27 @@
 <?php
 echo "here <br/>";
-$img_dir = 'paint_queue/';
+$img_dir = '~/paint_queue';
 $file_count = 0;
 $paintings = glob($img_dir.'*');
-echo "$file_count";
+echo "$file_count <br/>";
 if($paintings){
 	$file_count = count($paintings);
 }
 $file_count += 1;
 
-echo $_POST['imageurl'];
-$img = base64_decode($_POST['imageurl']);
-file_put_contents($img_dir.$file_count.'png', img);
+$imgData = $_POST['imageurl'];
+$imgData = substr($imgData,strpos($imgData,",")+1);
+$imgData = str_replace(' ', '+', $imgData);
+$img = base64_decode($imgData);
+echo "$img <br/>";
+
+$filename = $img_dir.'/'.$file_count.'.png';
+chmod($img_dir,0755);
+
+echo "$filename<br/>";
+
+$success = file_put_contents($filename, $img);
+
+echo "$success";
 
 ?>
