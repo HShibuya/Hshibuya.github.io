61c0011735a939503352cd058ed1364b77d3bed8
Hshibuya <hiroki.shibuya96@gmail.com>
2018-05-06 07:42:26 -0400

    test

--- php/uploadImage.php
+++ php/uploadImage.php
@@ -7,13 +7,11 @@ echo "$file_count <br/>";
 if($paintings){
 	$file_count = count($paintings);
 }
-$file_count += 1;
 
 $imgData = $_POST['imageurl'];
 $imgData = substr($imgData,strpos($imgData,",")+1);
 $imgData = str_replace(' ', '+', $imgData);
 $img = base64_decode($imgData);
-echo "$img <br/>";
 
 $filename = $img_dir.'/'.$file_count.'.png';
 chmod($img_dir,0755);
