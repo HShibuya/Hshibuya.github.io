ff2a02f77cc363d1a92c8c76a290eb41e98cafc8
Hshibuya <hiroki.shibuya96@gmail.com>
2018-05-09 04:11:13 -0400

    hallfame work

--- php/hallfame.php
+++ php/hallfame.php
@@ -26,73 +26,75 @@
 			</div>
 		</div>
 		
-		
-		<?php
-		function genDiv($row){
-			$pid = $row["pid"];
-			$name = $row["name"];
-			$title = $row["title"];
-			$desc = $row["descr"];
-			$date = $row["only_date"];
-			$comment = $row["comments"];
-			
-			$functStr = "showDesc('".$pid."','".$name."','".$title."','".$desc."','".$date."','".$comment."')";
-			
-			$html = '<div id="$pid" class="painting" onclick="'.$functStr.'">
-				<p class="title">'.$title.'</p>
-				<img class= "drawing" src="../paintings/'.$pid.'.png">
-				<p class="name"><i>By: '.$name.'</i></p>
-			</div>';
-			echo $html;
-		}
-		
-		//$servername = 'localhost';
-		$servername = 'localhost';
-		$username = 'id3110622_hshibuya96';
-		$password = 'boort7467';
-		$dbname = 'id3110622_paintings';
+		<div id='gallery'>
+			<?php
+			function genDiv($row){
+				$pid = $row["pid"];
+				$name = $row["name"];
+				$title = $row["title"];
+				$desc = $row["descr"];
+				$date = $row["only_date"];
+				$comment = $row["comments"];
 
-		$connection = new mysqli($servername, $username, $password, $dbname);
-		//$connection = new mysqli($servername);
-		
-		if ($connection->connect_error) {
-			echo("<script>console.log('Connection failed: ".$connection->connect_error."')</script>");
-		} 
-		else{
-			echo("<script>console.log('Connection successful')</script>");
-		}
-		
-		$sql = 'SELECT paintings.pid, name, title, descr, DATE(pdate) AS only_date, comments FROM paintings INNER JOIN approved ON paintings.pid=approved.pid ORDER BY pdate';
-		$results = $connection->query($sql);
-		if ($results->num_rows > 0) {
-			echo("<script>console.log('Query successful')</script>");
-			// output data of each row
-			while($row = $results->fetch_assoc()) {
-				genDiv($row);
+				$functStr = "showDesc('".$pid."','".$name."','".$title."','".$desc."','".$date."','".$comment."')";
+
+				$html = '<div id="$pid" class="painting" onclick="'.$functStr.'">
+					<p class="title">'.$title.'</p>
+					<img class= "drawing" src="../paintings/'.$pid.'.png">
+					<p class="name"><i>By: '.$name.'</i></p>
+				</div>';
+				echo $html;
 			}
-		}
-		else{
-			echo '<script>console.log("Error: '.$sql.'");console.log("'.$connection->error.'")</script>;';
-		}
-			
-		
-		?>
-		
-		
-		
-		<script type="text/javascript">
-			
-			//function to fill in the modal HTML for clicked painting
-			function showDesc(pid,name,title,descr,date,comment){
-				$('#zoom').css('display','block');
-				$('#imageUp').attr('src','../paintings/'+pid+'.png');
-				$('#title').html(title);
-				$('#nameUp').html('By: '+name);
-				$('#desc').html(descr);
-				$('#comments').html(comment);
-				$('#date').html(date);
+
+			//$servername = 'localhost';
+			$servername = 'localhost';
+			$username = 'id3110622_hshibuya96';
+			$password = 'boort7467';
+			$dbname = 'id3110622_paintings';
+
+			$connection = new mysqli($servername, $username, $password, $dbname);
+			//$connection = new mysqli($servername);
+
+			if ($connection->connect_error) {
+				echo("<script>console.log('Connection failed: ".$connection->connect_error."')</script>");
+			} 
+			else{
+				echo("<script>console.log('Connection successful')</script>");
 			}
-		</script>
+
+			$sql = 'SELECT paintings.pid, name, title, descr, DATE(pdate) AS only_date, comments FROM paintings INNER JOIN approved ON paintings.pid=approved.pid ORDER BY pdate';
+			$results = $connection->query($sql);
+			if ($results->num_rows > 0) {
+				echo("<script>console.log('Query successful')</script>");
+				// output data of each row
+				while($row = $results->fetch_assoc()) {
+					genDiv($row);
+				}
+			}
+			else{
+				echo '<script>console.log("Error: '.$sql.'");console.log("'.$connection->error.'")</script>;';
+			}
+
+
+			?>
+
+
+
+			<script type="text/javascript">
+
+				//function to fill in the modal HTML for clicked painting
+				function showDesc(pid,name,title,descr,date,comment){
+					$('#zoom').css('display','block');
+					$('#imageUp').attr('src','../paintings/'+pid+'.png');
+					$('#title').html(title);
+					$('#nameUp').html('By: '+name);
+					$('#desc').html(descr);
+					$('#comments').html(comment);
+					$('#date').html(date);
+				}
+			</script>
+			
+		</div>
 		
 	</body>
 </html>
