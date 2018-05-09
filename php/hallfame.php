878c4991c2943cd440b47308c3a71f7ea5878060
Hshibuya <hiroki.shibuya96@gmail.com>
2018-05-09 03:50:12 -0400

    hallfame work

--- php/hallfame.php
+++ php/hallfame.php
@@ -2,7 +2,7 @@
 	<head>
 		<title>Hall Of Fame</title>
 		<link rel="stylesheet" type="text/css" href="../css/hall.css">
-		
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	</head>
 	<body>
 		
